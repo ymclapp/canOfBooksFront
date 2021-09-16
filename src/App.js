ce220ea52a17e5b54b5abbd1eb63b9d1b501d312
ymcla <ymclapp@yahoo.com>
2021-09-15 20:31:01 -0500

    working on the get to work in Postman and keep all of my book/books/Books straight

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import Header from './Header';
 import Footer from './Footer';
 import Profile from './Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import axios from 'axios';
 // import axios from 'axios';
 // import BestBooks from './BestBooks';
 
 
 
 class App extends React.Component {
-  state = { user: null }
+  state = { books: null };
+
+  //Run fetch as soon as the component has loaded
+  componentDidMount() {
+    this.fetchBooks();
+
+   }
+
+   async fetchBooks() {
+     let apiURL = `${ process.env.REACT_APP_API_URL }/books`;
+     try {
+       let results = await axios.get(apiURL);
+       this.setState({ books:  results.data });
+     }
+     catch (err) {
+       console.log(err);
+     }
+   }
 
   
-  loginHandler = (user) => { this.setState({ user}); };
-  logoutHandler = () => { this.setState({ user: null }); };
+  // loginHandler = (user) => { this.setState({ user}); };
+  // logoutHandler = () => { this.setState({ user: null }); };
 
   render() {
     return (
@@ -28,9 +48,9 @@ class App extends React.Component {
             <nav>
                 <h1>Books - Books - Books</h1>
                 <Link to = "/">Home</Link>
-                {this.state.user && <Link to = "/profile">Profile</Link>}
-                <form onSubmit = {this.loginHandler}>
-                <Logout onLogout = {this.logoutHandler} />
+                {this.state.books && <Link to = "/profile">Profile</Link>}
+                {/* <form onSubmit = {this.loginHandler}>
+                <Logout onLogout = {this.logoutHandler} /> */}
             </nav>
 
           <Switch>
@@ -46,7 +66,7 @@ class App extends React.Component {
                 {/* } */}
             </Route>
             <Route path = "/profile">
-                <Profile user = {this.state.user} />
+                <Profile user = {this.state.books} />
             </Route> 
             <Route path = "/help">
                 <h1>Help Me!</h1>
